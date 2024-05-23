e668ada504f17ab7dbe84cedb700dc223823c1d1
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-05-23 08:39:20 -0300

    formulario de descricao de capitulos

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -6,6 +6,7 @@ import { ArrowLeft, LayoutDashboard } from "lucide-react";
 import { db } from "@/lib/db";
 import { IconBadge } from "@/components/icon-badge";
 import { ChapterTitleForm } from "./_componentes/chapter-title-form";
+import { ChapterDescriptionForm } from "./_componentes/chapter-description-form";
 
 const ChapterIdPage = async ({
     params,
@@ -76,6 +77,11 @@ const ChapterIdPage = async ({
                             </h2>
                         </div>
                         <ChapterTitleForm 
+                            initialData={chapter}
+                            courseId={params.courseId}
+                            chapterId={params.chapterId}
+                        />
+                        <ChapterDescriptionForm 
                             initialData={chapter}
                             courseId={params.courseId}
                             chapterId={params.chapterId}
