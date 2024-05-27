5699c1c6bfc44eaabcd070412698825bac85b5e3
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-05-27 10:33:20 -0300

    data table

--- app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -2,9 +2,19 @@
 
 import { Course } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
-import { ArrowUpDown, MoreHorizontal } from "lucide-react"
+import { ArrowUpDown, MoreHorizontal, Pencil } from "lucide-react"
+import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Badge } from "@/components/ui/badge";
+import { cn } from "@/lib/utils";
+import { parse } from "path";
 
 export const columns: ColumnDef<Course>[] = [
     {
@@ -34,6 +44,15 @@ export const columns: ColumnDef<Course>[] = [
                 </Button>
             )
         },
+        cell: ({ row }) => {
+            const price = parseFloat(row.getValue("price") || "0");
+            const formatted = new Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+            }).format(price);
+
+            return <div>{formatted}</div>;
+        }
     },
     {
         accessorKey: "isPublished",
@@ -48,5 +67,42 @@ export const columns: ColumnDef<Course>[] = [
                 </Button>
             )
         },
+        cell: ({ row }) => {
+            const isPublished = row.getValue("isPublished") || false;
+
+            return (
+                <Badge className={cn(
+                    "bg-slate-500",
+                    isPublished && "bg-sky-700"
+                )}>
+                    {isPublished ? "Sim" : "Não"}
+                </Badge>
+            )
+        }
     },
+    {
+        id: "actions",
+        cell: ({ row }) => {
+            const { id } = row.original;
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-4 w-8 p-0">
+                            <span className="sr-only">Abrir menu</span>
+                            <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                    <Link href={`/teacher/courses/${id}`}>
+                        <DropdownMenuItem>
+                            <Pencil className="h-4 w-4 mr-2" />
+                            Editar
+                        </DropdownMenuItem>
+                    </Link>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            )
+        }
+    }
 ];
--- app/(dashboard)/(routes)/teacher/courses/_components/data-table.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/data-table.tsx
@@ -3,13 +3,17 @@
 import * as React from "react";
 import {
     ColumnDef,
+    ColumnFiltersState,
     SortingState,
     flexRender,
     getCoreRowModel,
+    getFilteredRowModel,
     getPaginationRowModel,
     getSortedRowModel,
     useReactTable,
 } from "@tanstack/react-table";
+import Link from "next/link";
+import { PlusCircle } from "lucide-react";
 
 import {
     Table,
@@ -20,6 +24,7 @@ import {
     TableRow,
 } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[];
@@ -31,6 +36,8 @@ export function DataTable<TData, TValue>({
     data,
 }: DataTableProps<TData, TValue>) {
     const [sorting, setSorting] = React.useState<SortingState>([]);
+    const [columnFilters, setColumnFilters] =
+        React.useState<ColumnFiltersState>([]);
 
     const table = useReactTable({
         data,
@@ -39,13 +46,38 @@ export function DataTable<TData, TValue>({
         getPaginationRowModel: getPaginationRowModel(),
         onSortingChange: setSorting,
         getSortedRowModel: getSortedRowModel(),
+        onColumnFiltersChange: setColumnFilters,
+        getFilteredRowModel: getFilteredRowModel(),
         state: {
             sorting,
+            columnFilters,
         },
     });
 
     return (
         <div>
+            <div className="flex items-center py-4 justify-between">
+                <Input
+                    placeholder="Filtrar cursos..."
+                    value={
+                        (table
+                            .getColumn("title")
+                            ?.getFilterValue() as string) ?? ""
+                    }
+                    onChange={(event) =>
+                        table
+                            .getColumn("title")
+                            ?.setFilterValue(event.target.value)
+                    }
+                    className="max-w-sm"
+                />
+                <Link href="/teacher/create" >
+                    <Button>
+                        <PlusCircle className="h-4 w-4 mr-2" />
+                        Novo curso
+                    </Button>
+                </Link>
+            </div>
             <div className="rounded-md border">
                 <Table>
                     <TableHeader>
