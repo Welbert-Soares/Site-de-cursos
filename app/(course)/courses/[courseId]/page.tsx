1b4bed776ac4f6fa1542b38ec9a0174394518cdb
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-05-27 20:23:36 -0300

    sidebar mobile

---
+++ app/(course)/courses/[courseId]/page.tsx
@@ -0,0 +1,9 @@
+const CourseIdPage = () => {
+    return (
+        <div>
+            <h1>CourseIdPage</h1>
+        </div>
+    )
+}
+
+export default CourseIdPage
--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -12,22 +12,20 @@ interface SearchPageProps {
     searchParams: {
         title: string;
         categoryId: string;
-    }
-};
+    };
+}
 
-const SearchPage = async ({
-    searchParams
-}: SearchPageProps) => {
+const SearchPage = async ({ searchParams }: SearchPageProps) => {
     const { userId } = auth();
 
-    if(!userId) {
-        return redirect("/")
+    if (!userId) {
+        return redirect("/");
     }
 
     const categories = await db.category.findMany({
         orderBy: {
-            name: 'asc'
-        }
+            name: "asc",
+        },
     });
 
     const courses = await getCourses({
@@ -35,22 +33,17 @@ const SearchPage = async ({
         ...searchParams,
     });
 
-    return (  
+    return (
         <>
-        <div className="px-6 pt-6 md:hidden md:mb-0 block">
-            <SearchInput />
-        </div>
-        <div className="p-6 space-y-4">
-            <Categories 
-                items={categories}
-            />
-            <CoursesList 
-                items={courses} 
-            />
-        </div>
-        
+            <div className="px-6 pt-6 md:hidden md:mb-0 block">
+                <SearchInput />
+            </div>
+            <div className="p-6 space-y-4">
+                <Categories items={categories} />
+                <CoursesList items={courses} />
+            </div>
         </>
     );
-}
- 
+};
+
 export default SearchPage;
